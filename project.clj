e697178e0d8a098e8f0f1f02e937774087b7e827
Elliot Murphy <statik@users.noreply.github.com>
2015-04-10 08:38:57 -0400

    snapshot from dojo

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject clojure-dojo "0.1.0-SNAPSHOT"
+  :description "Code that solves http://golang-challenge.com/go-challenge1/"
+  :url "http://github.com/statik/clojure-dojo"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clojurewerkz/buffy "1.0.1"]
+                 [gloss "0.2.4"]
+                 [clj-mmap "1.1.2"]
+                 ])
